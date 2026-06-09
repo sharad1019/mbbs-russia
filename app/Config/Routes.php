5ca9d2cde99fa044c346e4439b2027c771139052
Satyam Gupta <Satyam@hire4event.com>
2026-06-09 15:43:05 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/kleido', 'Home::kleido');
+$routes->get('/route-to-india', 'Home::routeToIndia');
+$routes->get('/travxevent', 'Home::travxevent');
+$routes->get('/under-construction', 'Home::UnderConstruction');
